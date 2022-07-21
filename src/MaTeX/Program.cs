88d04628ceeec7cb08d1600f931fd01f33046569
Tom Schubart <schubarttom@googlemail.com>
2022-07-21 02:08:43 +0200

    MathToLatex angepasst

--- src/MaTeX/Program.cs
+++ src/MaTeX/Program.cs
@@ -7,61 +7,53 @@ namespace MaTeX
 {
     class Program
     {
-        static string MathToLatex(object o)
+        static string MathToLatex(Vector v) //Latexumwandlung für Vektoren
+        {                                                            
+            int m;
+            string Latex=@"\begin{pmatrix}{c}" + "\n";
+            //die einzelnen Zeilen des Vektors werden nun in Latex Schreibweise umgewandelt
+            for (m = 0; m < v.Count; m++)
             {
-                //zuerst Abfrage ob Vector
-                if (o is Vector)          
-                {                                                              
-                    int m;
-                    Vector V=(Vector)o;
-                    string Latex=@"\begin{pmatrix}{c}" + "\n";
-                    //die einzelnen Zeilen des Vektors werden nun in Latex Schreibweise umgewandelt
-                    for (m = 0; m < V.Count; m++)
-                    {
-                        Latex=Latex + Convert.ToString(V[m])+ @" \\" + "\n";
-                    }
-                return Latex + @"\end{pmatrix}";
-
-                //nun Abfrage ob Matrix
-                }else if(o is Matrix)
-                {
-                    int m;
-                    int n;
-                    Matrix M=(Matrix)o;
-                    string Latex=@"\begin{bmatrix}{rrr}" + "\n";
-                    //die einzelnen Zeilen der Matrix werden nun in Latex Schreibweise umgewandelt
-                    for (m = 0; m < M.RowCount; m++)                           
-                    {
-                        for (n = 0; n < M.ColumnCount-1; n++)
-                        {
-                            Latex=Latex + Convert.ToString(M[m,n])+" & ";
-                        }
-                        Latex=Latex + Convert.ToString(M[m,n]) + @" \\" + "\n";
-                    }
-                return Latex + @"\end{bmatrix}";
-
-                //zuletzt Abfrage ob String (Term, Gleichung)
-                }else if (o is string)
-                {
-                    string neu="";
-                    int i;
-                    string s = (string)o;
-                    //zunächst wird abgefragt ob der String eine Gleichung ist
-                    if (s.Contains("="))
-                    {
-                        string[] gleichungen = s.Split("=");
-                        for (i = 0; i < gleichungen.Length-1; i++)
-                        {
-                             neu = neu + Expr.Parse(gleichungen[i]).ToLaTeX() + "=";
-                        }
-                        return neu + Expr.Parse(gleichungen[i]).ToLaTeX();
-                    /* ist dies nicht der Fall, 
-                    so ist der String ein Term und kann einfach umgewandelt werden */
-                    }else return Expr.Parse(s).ToLaTeX();
-                    
-                //mögliche Erweiterungen
-                }else return null;
+                Latex=Latex + Convert.ToString(v[m])+ @" \\" + "\n";
             }
+        return Latex + @"\end{pmatrix}";
+        }
+
+        static string MathToLatex(Matrix m) //Latexumwandlung für Matrizen
+        {
+            int i;
+            int n;
+            string Latex=@"\begin{bmatrix}{rrr}" + "\n";
+            //die einzelnen Zeilen der Matrix werden nun in Latex Schreibweise umgewandelt
+            for (i = 0; i < m.RowCount; i++)                           
+            {
+                for (n = 0; n < m.ColumnCount-1; n++)
+                {
+                    Latex=Latex + Convert.ToString(m[i,n])+" & ";
+                }
+                Latex=Latex + Convert.ToString(m[i,n]) + @" \\" + "\n";
+            }
+        return Latex + @"\end{bmatrix}";
+        }
+
+        static string MathToLatex(String s) //Latexumwandlung für Terme und Gleichungen
+        {
+            string neu="";
+            int i;
+            //zunächst wird abgefragt ob der String eine Gleichung ist
+            if (s.Contains("="))
+            {
+                string[] gleichungen = s.Split("=");
+                for (i = 0; i < gleichungen.Length-1; i++)
+                {
+                     neu = neu + Expr.Parse(gleichungen[i]).ToLaTeX() + "=";
+                }
+                return neu + Expr.Parse(gleichungen[i]).ToLaTeX();
+            /* ist dies nicht der Fall, 
+            so ist der String ein Term und kann einfach umgewandelt werden */
+            }
+            else return Expr.Parse(s).ToLaTeX();
+        }
 
         static void Main(string[] args)
         {
