ebc6168705ee48a280b17d69748ba165fc2d5388
FabianBartl <fabian@informatic-freak.de>
2022-07-23 11:45:59 +0200

    matex namespace ausgelagert

--- src/MaTeX/Program.cs
+++ src/MaTeX/Program.cs
@@ -2,51 +2,11 @@
 using MathNet.Numerics.LinearAlgebra.Double;
 using Expr = MathNet.Symbolics.SymbolicExpression;
 
-namespace MaTeX
+//matex funktionen importieren
+using Mtx = MaTeX;
+
+namespace MyApp
 {
-    static public class Conv
-    {
-        static public string MathToLatex(Vector vec) //Latexumwandlung für Vektoren
-        {                                                            
-            string Latex = @"\begin{pmatrix}{c}" + "\n";
-            //die einzelnen Zeilen des Vektors werden nun in Latex Schreibweise umgewandelt
-            for (int row=0; row<vec.Count; row++) Latex += Convert.ToString(vec[row]) + @" \\" + "\n";
-            return Latex + @"\end{pmatrix}";
-        }
-
-        static public string MathToLatex(Matrix mtr) //Latexumwandlung für Matrizen
-        {
-            string Latex = @"\begin{bmatrix}{rrr}" + "\n";
-            //die einzelnen Zeilen der Matrix werden nun in Latex Schreibweise umgewandelt
-            for (int row=0, col=0; row<mtr.RowCount; row++)                           
-            {
-                for (col=0; col<mtr.ColumnCount-1; col++) Latex += Convert.ToString(mtr[row,col])+" & ";
-                Latex += Convert.ToString(mtr[row,col]) + @" \\" + "\n";
-            }
-            return Latex + @"\end{bmatrix}";
-        }
-
-        static public string MathToLatex(String str) //Latexumwandlung für Terme und Gleichungen
-        {
-            string neu = "";
-            int i;
-            //zunächst wird abgefragt ob der String eine Gleichung ist
-            if (str.Contains("="))
-            {
-                string[] gleichungen = str.Split("=");
-                for (i = 0; i < gleichungen.Length-1; i++)
-                {
-                     neu = neu + Expr.Parse(gleichungen[i]).ToLaTeX() + "=";
-                }
-                return neu + Expr.Parse(gleichungen[i]).ToLaTeX();
-            /* ist dies nicht der Fall, 
-            so ist der String ein Term und kann einfach umgewandelt werden */
-            }
-            else return Expr.Parse(str).ToLaTeX();
-        }
-
-    }
-
     class Program
     {
         static void Main()
@@ -58,9 +18,9 @@ namespace MaTeX
             {1,1,1},
             {1,2,3},
             {4,3,2}});
-            Console.WriteLine(Conv.MathToLatex(S));
-            Console.WriteLine(Conv.MathToLatex(A));
-            Console.WriteLine(Conv.MathToLatex(B));
+            Console.WriteLine(Mtx.Conv.MathToLatex(S));
+            Console.WriteLine(Mtx.Conv.MathToLatex(A));
+            Console.WriteLine(Mtx.Conv.MathToLatex(B));
         }
     }
 }
